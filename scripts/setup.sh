641f8b361dd1e29b1f634c716ca61876a654899a
yukang <ygchu7@163.com>
2024-04-25 12:17:40 +0800

    update filepath refers in scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
+data_root="../data"
+results_root="../results"
 rm -rf data
-ln -s /home/data_91_d/chuyg/mix-debias/data data
+ln -s ${data_root} data
 
 rm -rf ckpts
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/SpanBERT/ckpts" ]; then
-  mkdir -p /home/data_91_d/chuyg/mix-debias/SpanBERT/ckpts
+if [ ! -d "${results_root}/SpanBERT/ckpts" ]; then
+  mkdir -p ${results_root}/SpanBERT/ckpts
 fi
-ln -s /home/data_91_d/chuyg/mix-debias/SpanBERT/ckpts ckpts
+ln -s ${results_root}/SpanBERT/ckpts ckpts
